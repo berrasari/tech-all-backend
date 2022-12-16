efa6cbc7803fe2db5586342561da76742c6388e4
Ezginur UÇAN <84808486+nurezgiucn@users.noreply.github.com>
2022-12-16 23:00:28 +0300

    app.js

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ var cors = require('cors');
 app.use(cors());
 const userRouter = require('./api/users/user.router');
 const ContentRouter = require('./api/Contents/Content.router');
+const CategorytRouter = require('./api/Categories/categories.router');
 app.use(express.json());
 app.use("/api/users",userRouter);
 app.use("/api/Contents", ContentRouter);
-
+app.use("/api/Categories", CategorytRouter);
 
 app.listen(process.env.APP_PORT,()=>{
     console.log("listening", process.env.APP_PORT);
