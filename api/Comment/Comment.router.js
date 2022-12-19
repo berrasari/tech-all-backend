e972868c2ccf52262144b2263043d409e8f76374
Ezginur UÇAN <84808486+nurezgiucn@users.noreply.github.com>
2022-12-19 14:26:56 +0300

    Update Comment.router.js

--- api/Comment/Comment.router.js
+++ api/Comment/Comment.router.js
@@ -1,10 +1,9 @@
-const { createComment, getComments, getCommentByuser, getCommentById,getCount} = require("./Comment.controller");
+const { createComment, getComments, getCommentByuser, getCommentById} = require("./Comment.controller");
 const router = require("express").Router();
 
 router.post("/",createComment);
 router.get("/", getComments);
 router.get("/author/:username", getCommentByuser);
 router.get("/:ContentID", getCommentById);
-router.get("count/:Count", getCount);
 
 module.exports = router;
