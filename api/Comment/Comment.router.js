e81189f875f7bbc85433ff52f15884f4c3d58d2c
Ezginur UÇAN <84808486+nurezgiucn@users.noreply.github.com>
2022-12-20 04:34:05 +0300

    Delete api/Comment directory

--- api/Comment/Comment.router.js
+++
@@ -1,9 +0,0 @@
-const { createComment, getComments, getCommentByuser, getCommentById} = require("./Comment.controller");
-const router = require("express").Router();
-
-router.post("/",createComment);
-router.get("/", getComments);
-router.get("/author/:username", getCommentByuser);
-router.get("/:ContentID", getCommentById);
-
-module.exports = router;
